dda7bb24740e6e00e30ba39f78572b04726db3e1
hey24sheep <hey24sheep@gmail.com>
2021-08-23 15:03:00 +0530

    more perf

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ function initPortfolio() {
     // load portfolio markdown
     setTimeout(() => {
         generatePortfolioFromGithub();
+    });
+};
 
+function loadParticles() {
+    setTimeout(() => {
         // load header nexus particles
         loadHeaderNexusParticles();
 
         // load bg space particles
         loadSpaceParticles();
     });
-};
+}
--- src/projects.js
+++ src/projects.js
@@ -2,11 +2,15 @@ function initPortfolio() {
     // load project list markdown
     setTimeout(() => {
         generateProjectList();
+    });
+};
 
+function loadParticles() {
+    setTimeout(() => {
         // load header nexus particles
         loadHeaderNexusParticles();
 
         // load bg space particles
         loadSpaceParticles();
     });
-};
+}
